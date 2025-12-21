7f366037fcf9cd70e2c8740968b5186254ddb7b1
LukyGuyLucky <111160598+LukyGuyLucky@users.noreply.github.com>
2025-12-21 16:12:16 +0800

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,3 @@
+c3c compile-run handle1.c3 -l cfltk2.lib -l gdi32.lib -l gdiplus.lib -l user32.lib -l comctl32.lib -l comdlg32.lib  -l kernel32.lib -l fltk.lib -l fltk_gl.lib -l fltk_images.lib -l fltk_jpeg.lib -l fltk_png.lib -l fltk_z.lib -l winspool.lib -l ole32.lib --wincrt=none
+c3c compile-run image.c3 -l cfltk2.lib -l gdi32.lib -l gdiplus.lib -l user32.lib -l comctl32.lib -l comdlg32.lib  -l kernel32.lib -l fltk.lib -l fltk_gl.lib -l fltk_images.lib -l fltk_jpeg.lib -l fltk_png.lib -l fltk_z.lib -l winspool.lib -l ole32.lib --wincrt=none
+pause
